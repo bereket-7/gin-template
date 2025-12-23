782bb10c70d9005af76f81f5ea2ab98ffc633a8a
bereket-7 <bekhonelign@gmail.com>
2025-12-24 00:00:51 +0300

    feat: Initialize Gin application with routing, CORS middleware, configuration, Redis connection, and graceful shutdown.

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,7 +1,13 @@
 package app
 
 import (
+	"context"
 	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/bereket-7/gin-template/internal/config"
 	"github.com/bereket-7/gin-template/internal/logger"
@@ -43,5 +49,40 @@ func New() *App {
 
 func (a *App) Run() error {
 	address := fmt.Sprintf(":%s", a.config.Port)
-	return a.engine.Run(address)
+
+	srv := &http.Server{
+		Addr:    address,
+		Handler: a.engine,
+	}
+
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Log.Fatal("listen error", zap.Error(err))
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be caught, so no need to add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	logger.Log.Info("shutting down server...")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Log.Fatal("server forced to shutdown", zap.Error(err))
+		return err
+	}
+
+	logger.Log.Info("server exiting")
+	return nil
 }
