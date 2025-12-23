4c33110b3c2db039a3e2aee24dd8e4f11e030744
bereket-7 <bekhonelign@gmail.com>
2025-12-23 04:59:47 +0300

    router

---
+++ internal/router/router.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"github.com/bereket-7/gin-template/internal/handlers"
+	"github.com/bereket-7/gin-template/internal/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func Setup(r *gin.Engine) {
+	// Global middleware
+	r.Use(middleware.Logger())
+	r.Use(middleware.Recovery())
+
+	api := r.Group("/api")
+	{
+		api.GET("/health", handlers.HealthCheck)
+	}
+}
