4e3dd7467cc7de6a65830acb336fe295dcc37704
bereket-7 <bekhonelign@gmail.com>
2025-12-24 00:19:05 +0300

    feat: add core router setup with global middleware and a health check endpoint

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,14 +1,15 @@
 package router
 
 import (
+	"github.com/bereket-7/gin-template/internal/config"
 	"github.com/bereket-7/gin-template/internal/handlers"
 	"github.com/bereket-7/gin-template/internal/middleware"
 	"github.com/gin-gonic/gin"
 )
 
-func Setup(r *gin.Engine) {
+func Setup(r *gin.Engine, cfg *config.Config) {
 	// Global middleware
-	r.Use(middleware.CORS())
+	r.Use(middleware.CORS(cfg.AllowedOrigins))
 	r.Use(middleware.Logger())
 	r.Use(middleware.Recovery())
 
