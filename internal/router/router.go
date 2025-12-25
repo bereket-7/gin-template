b487246f597826d4e7bdb8ac16c6a869b7ad7bf7
bereket-7 <bekhonelign@gmail.com>
2025-12-25 03:27:09 +0300

    feat: add initial Gin router setup with middleware, health check endpoint, and Swagger documentation

--- internal/router/router.go
+++ internal/router/router.go
@@ -5,6 +5,9 @@ import (
 	"github.com/bereket-7/gin-template/internal/handlers"
 	"github.com/bereket-7/gin-template/internal/middleware"
 	"github.com/gin-gonic/gin"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func Setup(r *gin.Engine, cfg *config.Config) {
@@ -17,4 +20,6 @@ func Setup(r *gin.Engine, cfg *config.Config) {
 	{
 		api.GET("/health", handlers.HealthCheck)
 	}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 }
